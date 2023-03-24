17694533e7ebf46224e9c31b445a462b9696e754
n-matvey <matveyproyt12272008@gmail.com>
2023-03-24 21:52:59 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Modal from './components/Modal/Modal';
+import Button from './components/Button/Button';
+import TaskList from './components/TaskList/TaskList';
 
 function App() {
+  const [ show, setShow ] = useState(false);
+  const [ newTask, setNewTask ] = useState('');
+  const [ tasks, setTasks ] = useState([
+    {
+      id: 1,
+      title: 'Coding',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Eat',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Sleep',
+      completed: false
+    },
+    {
+      id: 4,
+      title: 'Coding',
+      completed: false
+    },
+
+  ])
+  const handleShow  = () => setShow(!show)
+
+  const handleChangeCheck = (event) => {
+    setNewTask(event.target.value);
+    console.log(newTask, 'new task');
+  }
+  const handleAddTask = () => {
+    setTasks((prevState) => [...prevState,
+      {
+        id: Math.floor((Math.random * 100)),
+        title: newTask,
+        completed: false
+      }]);
+    handleShow();
+  }
+
+  const handleDelete = (id) => {
+    const filter = tasks.filter(task => task.id !== id)
+      setTasks(filter)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {show && <Modal
+            handleChangeCheck={handleChangeCheck}
+            handleAdd={handleAddTask}
+            handleShow={handleShow}  />}
+
+        <Button handleClick={handleShow}>
+          Открыть модалку
+        </Button>
+
+
+        {/* task list */}
+        <TaskList
+            handleDelete={handleDelete}
+            list={tasks} />
+      </div>
   );
 }
 
